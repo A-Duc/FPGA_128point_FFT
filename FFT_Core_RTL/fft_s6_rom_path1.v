932c505ec066afffa3d4c75f370b8690a7bced0d
HuagDuck <hoanganhduc270905@gmail.com>
2026-04-17 14:08:23 +0700

    Modified ROM 2rd time - decrease error

--- FFT_Core_RTL/fft_s6_rom_path1.v
+++ FFT_Core_RTL/fft_s6_rom_path1.v
@@ -4,11 +4,11 @@ module fft_s6_rom_path1(
 );
 
     always @(*) begin
-    case (iAddress)
-        1'd0: oData = {2'd0, 24'h000000, 24'h000028}; // th=-0.00000 a=-0.00000 Ki=1.000000 rc=1.000000 ke=0.00E+00(0.00L) ae=0.00E+00 sig={0,0,0,0,0,0}
-        1'd1: oData = {2'd3, 24'h000000, 24'h000028}; // th=-1.57080 a=0.00000 Ki=1.000000 rc=1.000000 ke=0.00E+00(0.00L) ae=0.00E+00 sig={0,0,0,0,0,0}
-        default: oData = 50'h0;
-    endcase
-end
+        case (iAddress)
+            1'd0: oData = {2'd0, 24'h000000, 24'h000028};
+            1'd1: oData = {2'd3, 24'h000000, 24'h000028};
+            default: oData = 50'h0;
+        endcase
+    end
 
 endmodule
