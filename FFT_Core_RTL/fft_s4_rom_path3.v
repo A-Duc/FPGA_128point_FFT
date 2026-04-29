d5edc8edd9a25c57c31df765e19dbf9b6d47fefa
HuagDuck <hoanganhduc270905@gmail.com>
2026-04-30 06:36:38 +0700

    Regenerate hardware-aware CORDIC ROM for improved FFT accuracy

--- FFT_Core_RTL/fft_s4_rom_path3.v
+++ FFT_Core_RTL/fft_s4_rom_path3.v
@@ -5,14 +5,14 @@ module fft_s4_rom_path3(
 
     always @(*) begin
         case (iAddress)
-            3'd0: oData = {2'd0, 24'h000000, 24'h000028};
-            3'd1: oData = {2'd0, 24'hF2DF00, 24'h024967};
-            3'd2: oData = {2'd3, 24'h032F00, 24'h030D28};
-            3'd3: oData = {2'd3, 24'h0E3000, 24'h020CE8};
-            3'd4: oData = {2'd2, 24'h100000, 24'hC739A7};
-            3'd5: oData = {2'd2, 24'h02D000, 24'h020CE8};
-            3'd6: oData = {2'd2, 24'h0DE001, 24'h030D28};
-            3'd7: oData = {2'd1, 24'h1E3001, 24'h024967};
+            3'd0: oData = {2'd0,24'h000000,24'h000028};
+            3'd1: oData = {2'd0,24'hF2DF00,24'hC24967};
+            3'd2: oData = {2'd3,24'h1DEFE0,24'h863967};
+            3'd3: oData = {2'd3,24'h0E3100,24'h000CE8};
+            3'd4: oData = {2'd2,24'h2D2300,24'hC32D27};
+            3'd5: oData = {2'd2,24'h02DF00,24'h000CE8};
+            3'd6: oData = {2'd2,24'hF32120,24'h863967};
+            3'd7: oData = {2'd1,24'h1E3100,24'hC24967};
             default: oData = 50'h0;
         endcase
     end
